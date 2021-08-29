58fd02adb84f1424adad5e3844d334108b9f9574
dungyourluv <corepper03@gmail.com>
2021-08-29 23:25:46 +0700

    update

---
+++ assets/app.js
@@ -0,0 +1,290 @@
+function User(name, acc, pass, index) {
+  this.name = name;
+  this.acc = acc;
+  this.pass = pass;
+  this.index = index;
+}
+
+var DNA = new User(
+  "Đàm Ngọc Anh",
+  "DN200204@thpt-canloc-hatinh.edu.vn",
+  "Mk#anh0706",
+  1
+);
+var NTLA = new User(
+  "Nguyễn Thị Lan Anh",
+  "AnhNTL080304@thpt-canloc-hatinh.edu.vn",
+  "Mk#anh0707",
+  2
+);
+var VPA = new User(
+  "Võ Phi Anh",
+  "AnhVP050404@thpt-canloc-hatinh.edu.vn",
+  "Mk#anh0708",
+  3
+);
+var NTNA = new User(
+  "Nguyễn Thị Ái",
+  "AINT140704@thpt-canloc-hatinh.edu.vn",
+  "Mk#ai0709 ",
+  4
+);
+var LAC = new User(
+  "Lê Anh Chí",
+  "ChiLA250504@thpt-canloc-hatinh.edu.vn",
+  "Mk#chi0710",
+  5
+);
+var LTC = new User(
+  "Lê Thế Cường",
+  "CuongLT200404@thpt-canloc-hatinh.edu.vn",
+  "Mk#cuong0711",
+  6
+);
+var NTD = new User(
+  "Nguyễn Thị Dung",
+  "DungNT090104@thpt-canloc-hatinh.edu.vn",
+  "Mk#dung0712",
+  7
+);
+var NHD = new User(
+  "Nguyễn Huy Duy",
+  "DuyNH010604@thpt-canloc-hatinh.edu.vn",
+  "Mk#duy0713",
+  8
+);
+var LTD = new User(
+  "Lê Thế Dũng",
+  "DungLT200204@thpt-canloc-hatinh.edu.vn",
+  "Mk#dung0714",
+  9
+);
+var HVD = new User(
+  "Hồ Việt Đức",
+  " DucHV271004@thpt-canloc-hatinh.edu.vn ",
+  "Mk#duc0715",
+  10
+);
+var NSH = new User(
+  "Nguyễn Sỹ Hoàng",
+  "HoangNS100104@thpt-canloc-hatinh.edu.vn ",
+  "M#hoang0716 ",
+  11
+);
+var PBH = new User(
+  "Phạm Bảo Hòa",
+  "HoaPB190804@thpt-canloc-hatinh.edu.vn",
+  "Mk#hoa0717",
+  12
+);
+var NTTH = new User(
+  "Nguyễn Thị Thanh Huyền",
+  "HuyenNTT201004@thpt-canloc-hatinh.edu.vn ",
+  "Mk#huyen0718",
+  1
+);
+var TTTH = new User(
+  "Trần Thị Thu Huyền",
+  "HuyenTTT161104@thpt-canloc-hatinh.edu.vn ",
+  "Mk#huyen0719",
+  13
+);
+var DVH = new User(
+  "Đường Văn Hùng",
+  "HungDV190504@thpt-canloc-hatinh.edu.vn",
+  "Mk#hung0720 ",
+  14
+);
+var TXL = new User(
+  "Trần Xuân Lập",
+  "LapTX120104@thpt-canloc-hatinh.edu.vn",
+  "Mk#lap0721",
+  15
+);
+var TPL = new User(
+  "Trần Phương Lê",
+  "LeTTP060904@thpt-canloc-hatinh.edu.vn",
+  "Mk#1e0722 ",
+  16
+);
+var TDL = new User(
+  "Trần Đức Lương",
+  "LuongID100604@thpt-canloc-hatinh.edu.vn",
+  "Mk#luong0723",
+  17
+);
+var PTTM = new User(
+  "Phan Thị Tuyết Mai",
+  "MaiPTT021104@thpt-canloc-hatinh.edu.vn",
+  "Mk#mai0724",
+  18
+);
+var NCM = new User(
+  "Nguyễn Công Minh",
+  "MinhNC020904@thpt-canloc-hatinh.edu.vn",
+  "Mk#minh0725",
+  19
+);
+var DTTN = new User(
+  "Dương Thị Trang Nhung",
+  "NhungDTT050904@thpt-canloc-hatinh.edu.vn",
+  "Mk#nhung0726",
+  20
+);
+var CMQ = new User(
+  "Cao Minh Quang",
+  "QuangCM300804@thpt-canloc-hatinh.edu.vn",
+  "Mk#quang0727",
+  21
+);
+var PAT = new User(
+  "Phan Anh Tài",
+  "TaiPA160104@thpt-canloc-hatinh.edu.vn",
+  "Mk#tai0728",
+  22
+);
+var PTT = new User(
+  "Phạm Thị Tâm",
+  "TamPT020204@thpt-canloc-hatinh.edu.vn",
+  "Mk#tam0729",
+  23
+);
+var DTT = new User(
+  "Dương Trí Thắng",
+  "ThangDT011104@thpt-canloc-hatinh.edu.vn ",
+  "Mk#thang0730",
+  24
+);
+var PTHT = new User(
+  "Phan Thị Hoài Thu",
+  "ThuPTH170604@thpt-canloc-hatinh.edu.vn",
+  "Mk#thu0731 ",
+  25
+);
+var NBT = new User(
+  "Ngô Bá Thuật",
+  "ThuatNB240304@thpt-canloc-hatinh.edu.vn ",
+  "Mk#thuat0732",
+  26
+);
+var DTTT = new User(
+  "Đậu Thị Thu Thủy",
+  "ThuyDTT220104@thpt-canloc-hatinh.edu.vn",
+  "Mk#thuy0733 ",
+  27
+);
+var TTTT = new User(
+  "Trần Thị Thu Thủy",
+  "ThuyTTT210904@thpt-tanloc-hatinh.edu.vn ",
+  "Mk#thuy0734 ",
+  28
+);
+var PTT = new User(
+  "Phạm Thị Tình",
+  "TinhPT200804@thpt-canloc-hatinh.edu.vn",
+  "#tinh0735 ",
+  29
+);
+var DTT = new User(
+  "Dương Trí Toàn",
+  "ToanDT250104@thpt-canloc-hatinh.edu.vn ",
+  "Mk#toan0736 ",
+  30
+);
+var dung = new User(
+  "Thằng này",
+  "TOND150204@thpt-canloc-hatinh.edu.vn ",
+  "Mk#010737",
+  31
+);
+var HTQT = new User(
+  "Hoàng Thị Quỳnh Trang",
+  "TrangHTQ110104@thpt-canloc-hatinh.edu.vn",
+  "Mk#trang0738 ",
+  32
+);
+var NTT = new User(
+  "Nguyễn Thị Trang",
+  "TrangNT150804@thpt-canloc-hatinh.edu.vn ",
+  "Mk#trang0739 ",
+  33
+);
+var DTT = new User(
+  "Dương Trí Trung",
+  "TrungDT050604@thpt-canloc-hatinh.edu.vn",
+  "Mk#trung0740 ",
+  34
+);
+var HAT = new User(
+  "Hoàng Anh Tuấn",
+  "TuanHA180904@thpt-canloc-hatinh.edu.vn",
+  "Mk#tuan0741",
+  35
+);
+
+var array = [
+  DNA,
+  NTLA,
+  VPA,
+  NTNA,
+  LAC,
+  LTC,
+  NTD,
+  NHD,
+  LTD,
+  HVD,
+  NSH,
+  PBH,
+  NTTH,
+  TTTH,
+  DVH,
+  TXL,
+  TPL,
+  TDL,
+  PTTM,
+  NCM,
+  DTTN,
+  CMQ,
+  PAT,
+  PTT,
+  DTT,
+  PTHT,
+  NBT,
+  DTTT,
+  TTTT,
+  PTT,
+  DTT,
+  dung,
+  HTQT,
+  NTT,
+  DTT,
+  HAT,
+];
+var userName = "",
+  userAcc = "",
+  userPass = "",
+  html = "";
+var input = document.querySelector(".input");
+var btn = document.querySelector(".btn");
+var list = document.querySelector(".box-list");
+
+btn.onclick = function () {
+  if (input.value !== "") {
+    run();
+  }
+};
+
+function run() {
+  array.forEach(function (user) {
+    var valueInput = input.value;
+    if (user.name === valueInput) {
+      userName = user.name;
+      userAcc = user.acc;
+      userPass = user.pass;
+      html = ` <li class="box-item ">Tên: <span class="color">${userName}</span></li>
+      <li class="box-item">Tài Khoản: <span class="color">${userAcc}</span> </li>
+      <li class="box-item">Mật Khẩu: <span class="color">${userPass}</span></li>`;
+      list.innerHTML = html;
+    }
+  });
+}
